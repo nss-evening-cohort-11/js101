f431de3e349740f36723ee7611824b46e9cd6896
Zoe Ames <zoeames@gmail.com>
2020-01-20 18:51:28 -0600

    added dog breed function

--- functions.js
+++ functions.js
@@ -11,3 +11,10 @@ console.log(nuggetizer('pig'));
 console.log(nuggetizer('cow'));
 console.log(nuggetizer('fish'));
 console.log(nuggetizer('tire'));
+
+const dogBreed = (dog) => {
+  return `my favorite dog breed is ${dog}`;
+};
+
+console.log(dogBreed('lab'));
+console.log(dogBreed('actually a cat'));
