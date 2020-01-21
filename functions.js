76751d3374f1e62b6d3cbf9acb6610f2a1bed5d4
Zoe Ames <zoeames@gmail.com>
2020-01-20 18:47:33 -0600

    wrote nuggetizer function

---
+++ functions.js
@@ -0,0 +1,13 @@
+console.log('FUNCTIONS');
+
+const nuggetizer = (animal) => {
+  const output = `processed ${animal}`;
+  // const output = 'processed ' + animal;
+  return output;
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('tire'));
